b29537bf121e720e37bca231db3556580a4b949b
AdolfoLaviana <adolfoalllaviana@gmail.com>
2017-03-07 17:55:15 +0100

    Revert "asdfasd"
    This reverts commit e15ac8ce3185cb32cfae6e4a3e7cff683011842b.

--- www/js/routes.js
+++ www/js/routes.js
@@ -7,6 +7,8 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
+    
+  
 
       .state('menu', {
     url: '/page2',
