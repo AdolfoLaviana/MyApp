d5465eae7bc9e3c8da1e97302dcd79ecc8abbedd
AdolfoLaviana <adolfoalllaviana@gmail.com>
2017-03-07 14:11:38 +0100

    configuracion vistas
    configuracion vistas 4k hd

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,40 +5,37 @@ angular.module('app.controllers', [])
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+$scope.menuType = true;
+$scope.objectType = false;
+console.log("menuType menu"+$scope.menuType);
+console.log("objectType menu"+$scope.objectType);
+$scope.objectform = function(){
+	$scope.menuType = false;
+	$scope.objectType = true;
+	console.log("menuType object"+$scope.menuType);
+	console.log("objectType object"+$scope.objectType);
+}
+$scope.menuform = function(){
+	$scope.menuType = true;
+	$scope.objectType = false;
+	console.log("menuType object"+$scope.menuType);
+	console.log("objectType object"+$scope.objectType);
+}
 
 }])
    
-.controller('houseCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
    
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+console.log("menuType login"+$scope.menuType);
+console.log("objectType login"+$scope.objectType);
 
-
-}])
-   
-.controller('carCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
 
 
 }])
    
-.controller('otherCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
    
 .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
